ab751e033c9b08280fabe198dff9492fc761160e
vamsi95 <126490784+vamsi95@users.noreply.github.com>
2025-10-29 12:59:34 +0530

    Implement getFirstElement function in Task1.c
    Add function to get the first element of an array

--- Task1.c
+++ Task1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int getFirstElement(int arr[], int size) {
+    // Constant time operation
+    if (size == 0)
+        return -1;  // return -1 for empty array
+    return arr[0];
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("First element: %d\n", getFirstElement(arr, size));
+    return 0;
+}
